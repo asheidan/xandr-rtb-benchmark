7706fc0ccb1e3587af5125fbb01668ce0c5dd0e8
Emil Eriksson <asheidan@gmail.com>
2021-02-03 11:01:34 +0100

    Replace static wait on socket in testbench

--- testbench.sh
+++ testbench.sh
@@ -10,10 +10,12 @@ max_filehandles=$(jq -s 'max * 2' <<< $*)
 
 echo "Setting ulimit to ${max_filehandles}" >&2
 ulimit -n $max_filehandles
+
 ./$implementation &
 implementation_pid=$!
 
-sleep 1
+# Wait for implementation to be listening
+until nc -z 127.0.0.1 8080; do sleep 0.2; done
 
 # Make sure the implementation is killed if we exit
 trap "kill $implementation_pid" EXIT
